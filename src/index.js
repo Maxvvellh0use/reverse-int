e123ed35a6ae99117f51ee534aca537eb58e15ce
Maksim Pavlov <pavlovmark100@gmail.com>
2020-02-20 18:53:55 +0400

    feat: add solution reverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n < 0) {
+        const result = String(n).split('');
+        result.shift();
+        const revNum = result.reverse();
+        return Number(revNum.join(''))
+    }
+    const result = Number(String(n).split('').reverse().join(''));
+    return result;
+};
